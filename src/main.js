3647a7eacaa026694b589cb1eacdc45d8aff946d
Kattei <86080736+Kattei@users.noreply.github.com>
2021-07-25 11:02:33 -0500

    pagina principal

--- src/main.js
+++ src/main.js
@@ -4,3 +4,32 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+window.onload = function() {
+    animateprogress("bar",100);
+}
+
+function animateprogress (bar,value) { 
+    let getRequestAnimationFrame = function () {
+        return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function (){
+            window.setTimeout(enroute, 1 / 60 * 1000);
+        };
+         
+    };
+
+    let fpAnimationFrame = getRequestAnimationFrame(); 
+    let i = 0;
+    let animacion = function () {
+             
+    if (i<=val)
+        {
+            document.querySelector(bar).setAttribute("value",i);  
+            i++;
+            fpAnimationFrame(animacion);
+        }
+    }
+
+    fpAnimationFrame(animacion);
+}
