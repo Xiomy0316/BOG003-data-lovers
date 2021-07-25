7e24edbb0d21cc168fd5c9a8b8c29798ad480a0c
Kattei <86080736+Kattei@users.noreply.github.com>
2021-07-25 11:22:38 -0500

    Cambios en la barra de carga

--- src/main.js
+++ src/main.js
@@ -5,31 +5,7 @@ import data from './data/pokemon/pokemon.js';
 
 console.log(example, data);
 
-window.onload = function() {
-    animateprogress("bar",100);
-}
-
-function animateprogress (bar,value) { 
-    let getRequestAnimationFrame = function () {
-        return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        function (){
-            window.setTimeout(enroute, 1 / 60 * 1000);
-        };
-         
-    };
-
-    let fpAnimationFrame = getRequestAnimationFrame(); 
-    let i = 0;
-    let animacion = function () {
-             
-    if (i<=val)
-        {
-            document.querySelector(bar).setAttribute("value",i);  
-            i++;
-            fpAnimationFrame(animacion);
-        }
-    }
-
-    fpAnimationFrame(animacion);
-}
+let progress_bar = document.getElementById ("welcome");
+setTimeout (function() {
+    progress_bar.classList.add ("close");
+},9000);
