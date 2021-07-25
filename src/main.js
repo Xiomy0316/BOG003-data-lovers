6f4dfee5ff0fd436c072459322fe00fe7ca3646c
Xiomara <xiomarach2000@gmail.com>
2021-07-25 11:05:11 -0500

    Se modificó la funcionalidad de la barra de progreso

--- src/main.js
+++ src/main.js
@@ -13,35 +13,7 @@ console.log(frutas[0]);*/
 /*document.getElementById("welcome").style.display = "none";
 document.getElementById("pokemons").style.display = "block";*/
 
-window.onload = function() {
-    animateprogress("bar",100);
-}
-
-function animateprogress (bar,value) { 
-    let getRequestAnimationFrame = function () {
-        return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        function (){
-            window.setTimeout(enroute, 1 / 60 * 1000);
-        };
-         
-    };
-
-    let fpAnimationFrame = getRequestAnimationFrame(); 
-    let i = 0;
-    let animacion = function () {
-             
-    if (i<=val)
-        {
-            document.querySelector(bar).setAttribute("value",i);  
-            i++;
-            fpAnimationFrame(animacion);
-        }
-    }
-
-    fpAnimationFrame(animacion);
-}
-
-   /*let container = document.getElementById ("bar");
-
-    container.style ="hidden"*/ 
+let progress_bar = document.getElementById ("welcome");
+setTimeout (function() {
+    progress_bar.classList.add ("close");
+},9000);
