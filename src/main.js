b50143efb6213b2985ee3db794f789986ad345f4
Kattei <86080736+Kattei@users.noreply.github.com>
2021-08-02 16:44:02 -0500

    modificación en el color de tarjetas por tipo

--- src/main.js
+++ src/main.js
@@ -6,14 +6,20 @@ import data from './data/pokemon/pokemon.js';
 console.log(example, data);
 
 
+//funcion de carga
+let progress_bar = document.getElementById("welcome");
+setTimeout(function () {
+    progress_bar.classList.add("close");
+// funcion y llamado de las tarjetas
+    const pokemons = data.pokemon;
+    console.table(pokemons);
 
-let progress_bar = document.getElementById ("welcome");
-setTimeout (function() {
-    progress_bar.classList.add ("close");
-    document.getElementById("items").style.display = "block";
+    mostrarcard(pokemons);
 
-const pokemons = data.pokemon;
-console.table(pokemons);
+
+}, 9000);
+
+//creacion de las tarjetas
 
 const items = document.getElementById("items");
 const templateCard = document.getElementById("pokemon_card").content;
@@ -22,21 +28,27 @@ const fragment = document.createDocumentFragment();
 
 const mostrarcard = pokemons => {
     pokemons.forEach(pokemon => {
+        let clone = document.getElementById("pokemon_card").content;
         templateCard.querySelector("h2").textContent = pokemon.name
         templateCard.querySelector("img").setAttribute("src", pokemon.img)
         templateCard.querySelector("span").textContent = pokemon.num
-        templateCard.querySelector("small").textContent = pokemon.type
-        const clone = templateCard.cloneNode(true)
+        // divicion para recuadros de los tipos
+        templateCard.querySelector(".typepoke").textContent = pokemon.type[0]
+        if (pokemon.type.length >= 1) {
+            templateCard.querySelector(".secondpoke").textContent = pokemon.type[1]
+        }else (pokemon.type.length < 2) 
+        {
+            templateCard.querySelector(".secondpoke").style.display = "none" + pokemon.type[1]
+        }
+          // cambio de color de la tarjeta segun tipo
+        templateCard.querySelector("div .card").className = "card " + pokemon.type[0]
+        const clone1 = clone.cloneNode(true)
 
-        fragment.appendChild(clone)
+        fragment.appendChild(clone1)
     });
     items.appendChild(fragment);
 };
-mostrarcard(pokemons);
 
-
-
-},9000);
 
 
 
