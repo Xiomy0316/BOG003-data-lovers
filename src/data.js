1563dca6bcebb2191c835ee38c1f6391b02cd009
Xiomara <xiomarach2000@gmail.com>
2021-08-30 14:47:42 -0500

    Se suprimierón algunas posibles funcionalidades que estaban comentadas.

--- src/data.js
+++ src/data.js
@@ -32,29 +32,3 @@ export const filterOrderNum = (pokemons) => {
   });
 
 };
-
-/*export const prom = (pokemons) => {
-  return pokemons.map(function (poke) {
-    const attack = poke.stats["base-attack"];
-    //const defense = poke.stats["base-defense"];
-    //let dateTotal = (attack + defense) / 2;
-    //let datoCalculation = (((attack - dateTotal) ** 2 + (defense - dateTotal) ** 2) / 2);
-    // poke["Total"] = dateTotal.toFixed(2);
-    //poke["talculation"] = datoCalculation.toFixed();
-  return attack;
-  })
-}*/
-
-export const prom = (pokemons, key) => {
-  const sumPoke = pokemons.reduce(0, pokemons.stats[key])
-  return console.log(sumPoke);   
-}
-
-
-/*export const filterWeaknesses = (pokemons, weaknessesFilter) => {
-return pokemons.filter((poke)=>(poke.weaknesses.includes(weaknessesFilter)));
-};*/
-
-/*export const cleanPokemon = function limpiar() {
-  document.getElementById("type-select").reset();
-}*/
